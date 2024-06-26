f50bf4d7f360f0f27d08bc13a4621ed6eb77ddca
Bors Alexandru <bors.alexandru2004@gmail.com>
2024-06-26 20:25:52 +0300

    innitial commot

---
+++ DataBaseConectionExample/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace DataBaseConectionExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.user_name_add_TB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.user_surname_add_TB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.add_GB = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.users_LW = new System.Windows.Forms.ListView();
+            this.user_id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.user_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.user_surname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.add_BTN = new System.Windows.Forms.Button();
+            this.update_GB = new System.Windows.Forms.GroupBox();
+            this.update_BTN = new System.Windows.Forms.Button();
+            this.user_name_update_TB = new System.Windows.Forms.TextBox();
+            this.user_surname_update_TB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.delete_BTN = new System.Windows.Forms.Button();
+            this.add_GB.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.update_GB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // user_name_add_TB
+            // 
+            this.user_name_add_TB.Location = new System.Drawing.Point(94, 32);
+            this.user_name_add_TB.Name = "user_name_add_TB";
+            this.user_name_add_TB.Size = new System.Drawing.Size(151, 20);
+            this.user_name_add_TB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // user_surname_add_TB
+            // 
+            this.user_surname_add_TB.Location = new System.Drawing.Point(94, 58);
+            this.user_surname_add_TB.Name = "user_surname_add_TB";
+            this.user_surname_add_TB.Size = new System.Drawing.Size(151, 20);
+            this.user_surname_add_TB.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Surname";
+            // 
+            // add_GB
+            // 
+            this.add_GB.Controls.Add(this.add_BTN);
+            this.add_GB.Controls.Add(this.user_name_add_TB);
+            this.add_GB.Controls.Add(this.user_surname_add_TB);
+            this.add_GB.Controls.Add(this.label2);
+            this.add_GB.Controls.Add(this.label3);
+            this.add_GB.Location = new System.Drawing.Point(12, 12);
+            this.add_GB.Name = "add_GB";
+            this.add_GB.Size = new System.Drawing.Size(271, 122);
+            this.add_GB.TabIndex = 6;
+            this.add_GB.TabStop = false;
+            this.add_GB.Text = "Insert";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.delete_BTN);
+            this.groupBox2.Controls.Add(this.users_LW);
+            this.groupBox2.Location = new System.Drawing.Point(12, 140);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(271, 231);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tables";
+            // 
+            // users_LW
+            // 
+            this.users_LW.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.user_id,
+            this.user_name,
+            this.user_surname});
+            this.users_LW.FullRowSelect = true;
+            this.users_LW.HideSelection = false;
+            this.users_LW.Location = new System.Drawing.Point(6, 19);
+            this.users_LW.MultiSelect = false;
+            this.users_LW.Name = "users_LW";
+            this.users_LW.Size = new System.Drawing.Size(259, 177);
+            this.users_LW.TabIndex = 0;
+            this.users_LW.UseCompatibleStateImageBehavior = false;
+            this.users_LW.View = System.Windows.Forms.View.Details;
+            this.users_LW.SelectedIndexChanged += new System.EventHandler(this.users_LW_SelectedIndexChanged);
+            // 
+            // user_id
+            // 
+            this.user_id.Text = "ID";
+            this.user_id.Width = 29;
+            // 
+            // user_name
+            // 
+            this.user_name.Text = "Name";
+            this.user_name.Width = 111;
+            // 
+            // user_surname
+            // 
+            this.user_surname.Text = "Surname";
+            this.user_surname.Width = 115;
+            // 
+            // add_BTN
+            // 
+            this.add_BTN.Location = new System.Drawing.Point(94, 84);
+            this.add_BTN.Name = "add_BTN";
+            this.add_BTN.Size = new System.Drawing.Size(151, 23);
+            this.add_BTN.TabIndex = 8;
+            this.add_BTN.Text = "Add";
+            this.add_BTN.UseVisualStyleBackColor = true;
+            this.add_BTN.Click += new System.EventHandler(this.add_BTN_Click);
+            // 
+            // update_GB
+            // 
+            this.update_GB.Controls.Add(this.update_BTN);
+            this.update_GB.Controls.Add(this.user_name_update_TB);
+            this.update_GB.Controls.Add(this.user_surname_update_TB);
+            this.update_GB.Controls.Add(this.label1);
+            this.update_GB.Controls.Add(this.label4);
+            this.update_GB.Location = new System.Drawing.Point(300, 12);
+            this.update_GB.Name = "update_GB";
+            this.update_GB.Size = new System.Drawing.Size(271, 122);
+            this.update_GB.TabIndex = 9;
+            this.update_GB.TabStop = false;
+            this.update_GB.Text = "Update";
+            // 
+            // update_BTN
+            // 
+            this.update_BTN.Enabled = false;
+            this.update_BTN.Location = new System.Drawing.Point(94, 84);
+            this.update_BTN.Name = "update_BTN";
+            this.update_BTN.Size = new System.Drawing.Size(151, 23);
+            this.update_BTN.TabIndex = 8;
+            this.update_BTN.Text = "Update";
+            this.update_BTN.UseVisualStyleBackColor = true;
+            this.update_BTN.Click += new System.EventHandler(this.update_BTN_Click);
+            // 
+            // user_name_update_TB
+            // 
+            this.user_name_update_TB.Enabled = false;
+            this.user_name_update_TB.Location = new System.Drawing.Point(94, 32);
+            this.user_name_update_TB.Name = "user_name_update_TB";
+            this.user_name_update_TB.Size = new System.Drawing.Size(151, 20);
+            this.user_name_update_TB.TabIndex = 3;
+            // 
+            // user_surname_update_TB
+            // 
+            this.user_surname_update_TB.Enabled = false;
+            this.user_surname_update_TB.Location = new System.Drawing.Point(94, 58);
+            this.user_surname_update_TB.Name = "user_surname_update_TB";
+            this.user_surname_update_TB.Size = new System.Drawing.Size(151, 20);
+            this.user_surname_update_TB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Surname";
+            // 
+            // delete_BTN
+            // 
+            this.delete_BTN.Enabled = false;
+            this.delete_BTN.Location = new System.Drawing.Point(114, 202);
+            this.delete_BTN.Name = "delete_BTN";
+            this.delete_BTN.Size = new System.Drawing.Size(151, 23);
+            this.delete_BTN.TabIndex = 9;
+            this.delete_BTN.Text = "Delete";
+            this.delete_BTN.UseVisualStyleBackColor = true;
+            this.delete_BTN.Click += new System.EventHandler(this.delete_BTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.update_GB);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.add_GB);
+            this.Name = "Form1";
+            this.Text = "App";
+            this.add_GB.ResumeLayout(false);
+            this.add_GB.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.update_GB.ResumeLayout(false);
+            this.update_GB.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox user_name_add_TB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox user_surname_add_TB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox add_GB;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView users_LW;
+        private System.Windows.Forms.ColumnHeader user_id;
+        private System.Windows.Forms.ColumnHeader user_name;
+        private System.Windows.Forms.ColumnHeader user_surname;
+        private System.Windows.Forms.Button add_BTN;
+        private System.Windows.Forms.Button delete_BTN;
+        private System.Windows.Forms.GroupBox update_GB;
+        private System.Windows.Forms.Button update_BTN;
+        private System.Windows.Forms.TextBox user_name_update_TB;
+        private System.Windows.Forms.TextBox user_surname_update_TB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DataBaseConectionExample/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBaseConectionExample
+{
+    public partial class Form1 : Form
+    {
+        ExampleDataBaseEntities entities = new ExampleDataBaseEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            updateUserListView();
+            
+        }
+
+        private void add_BTN_Click(object sender, EventArgs e)
+        {
+
+            if (AreAllTextBoxesFromGroupBoxValid(add_GB))
+            {
+                add_BTN.Enabled = false;
+                string addBtnOldText = add_BTN.Text;
+                add_BTN.Text = "Please Wait...";
+
+                Users user = new Users();
+                user.user_name = user_name_add_TB.Text;
+                user.user_surname = user_surname_add_TB.Text;
+                entities.Users.Add(user);
+                entities.SaveChanges();
+
+                ClearAllTextBoxesFromGropBox(add_GB);
+                updateUserListView();
+                add_BTN.Text = addBtnOldText;
+                add_BTN.Enabled = true;
+                MessageBox.Show("Added with succes!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid Values!");
+            }
+        }
+
+        private void updateUserListView()
+        {
+            users_LW.SelectedItems.Clear();
+            users_LW.Items.Clear();
+            var users = from user in entities.Users
+                        //where true = true
+                        select user;
+            foreach ( var user in users )
+            {
+                ListViewItem listViewItem = new ListViewItem(user.user_id.ToString());
+                listViewItem.SubItems.Add(user.user_name.ToString());
+                listViewItem.SubItems.Add(user.user_surname.ToString());
+
+                users_LW.Items.Add(listViewItem);
+            }
+        }
+
+        int selectedUserId = -1;
+        private void users_LW_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (users_LW.SelectedItems.Count > 0)
+            {
+                selectedUserId = int.Parse(users_LW.SelectedItems[0].SubItems[0].Text);
+                var user = entities.Users.SingleOrDefault(u => u.user_id == selectedUserId);
+
+                user_name_update_TB.Text = user.user_name;
+                user_surname_update_TB.Text = user.user_surname;
+
+                EnableUpdateGB(true);
+                EnableDeleteGB(true);
+            }
+            else
+            {
+                selectedUserId = -1;
+
+                ClearAllTextBoxesFromGropBox(update_GB);
+
+                EnableUpdateGB(false);
+                EnableDeleteGB(false);
+            }
+        }
+
+        private void delete_BTN_Click(object sender, EventArgs e)
+        {
+            if(selectedUserId != -1)
+            {
+                var user = entities.Users.SingleOrDefault(u => u.user_id == selectedUserId);
+               
+                entities.Users.Remove(user);
+                entities.SaveChanges();
+                MessageBox.Show("Deleted with succes!");
+                updateUserListView();
+            }
+            else
+            {
+                MessageBox.Show("User not Selected!");
+            }
+        }
+        
+        private void update_BTN_Click(object sender, EventArgs e)
+        {
+            if(selectedUserId != -1)
+            {
+                if (AreAllTextBoxesFromGroupBoxValid(update_GB))
+                {
+                    update_BTN.Enabled = false;
+                    string btnOldText = update_BTN.Text;
+                    update_BTN.Text = "Please Wait...";
+
+                    var user = entities.Users.SingleOrDefault(u => u.user_id == selectedUserId);
+                    user.user_name = user_name_update_TB.Text;
+                    user.user_surname = user_surname_update_TB.Text;
+                    entities.SaveChanges();
+
+                    ClearAllTextBoxesFromGropBox(update_GB);
+                    updateUserListView();
+                    update_BTN.Text = btnOldText;
+                    //update_BTN.Enabled = true;
+                    MessageBox.Show("Updated with succes!");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Values!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("User not Selected!");
+            }
+        }
+       
+        private void EnableUpdateGB(bool boolean)
+        {
+            List<TextBox> textBoxes = update_GB.Controls.OfType<TextBox>().ToList();
+            List<Button> buttons = update_GB.Controls.OfType<Button>().ToList();
+
+            foreach(TextBox textBox in textBoxes)
+            {
+                textBox.Enabled = boolean;
+            }
+
+            foreach(Button button in buttons)
+            {
+                button.Enabled = boolean;
+            }
+        }
+
+        private void EnableDeleteGB(bool boolean)
+        {
+            delete_BTN.Enabled = boolean;
+        }
+
+        private bool AreAllTextBoxesFromGroupBoxValid(GroupBox groupBox)
+        {
+            List<TextBox> textBoxes = groupBox.Controls.OfType<TextBox>().ToList();
+            foreach (TextBox textBox in textBoxes)
+            {
+                if (!IsTextBoxTextValid(textBox.Text))
+                    return false;
+            }
+            return true;
+        }
+
+        private void ClearAllTextBoxesFromGropBox(GroupBox groupBox)
+        {
+            List<TextBox> textBoxes = groupBox.Controls.OfType<TextBox>().ToList();
+            foreach (TextBox textBox in textBoxes)
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private bool IsTextBoxTextValid(string text)
+        {
+            if (text.Equals("") || text.Equals(null))
+                return false;
+            return true;
+        }
+
+        
+    }
+}
